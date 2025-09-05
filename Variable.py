c1bd96af598fce8528fa146ec70418e339ad6c6b
Adarsha B M <adarsha23961@gmail.com>
2025-09-05 15:34:18 +0530

    Create Variable.py

---
+++ Variable.py
@@ -0,0 +1,19 @@
+age = 45 #integer
+
+account_balance = 324.45 #floating point
+
+name = "adarsha" #string
+
+is_accepted_terms_and_condition = True or False#boolean
+
+print(name)
+
+
+#Note=============
+variable type
+
+integer    393,3454,45654,4
+floating point(decimals)    1.34.5.7765.4
+Strings(text)  "adasg" ,'harshitha"
+Bolean(yes/no)      True or False
+#=============
