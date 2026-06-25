fa52cf53e83390616a827dd5e039a26fe08d4aa0
Adarsha B M <adarsha23961@gmail.com>
2026-06-25 16:26:35 +0530

    Update List.py

--- List.py
+++ List.py
@@ -6,7 +6,7 @@ item3 = "milk"
 print(item1, item2, item3)    
 
 items = ["bru", "sugar", "milk", "bru2"]   
-l = [1 , "bru", True, [1,2,3]]
+l = [1 , "bru", True, [1,2,3]]   
 print(l)
 print(items)
 print(items[0])
